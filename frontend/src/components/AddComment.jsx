e4cd6635d4be8f5250f7a9c9056f494946d4af22
asimsinan <asimyuksel@sdu.edu.tr>
2024-12-26 15:43:20 +0300

    son

--- frontend/src/components/AddComment.jsx
+++ frontend/src/components/AddComment.jsx
@@ -1,10 +1,31 @@
 import Header from "./Header";
 import React from "react";
-import { useLocation} from "react-router-dom";
+import { useLocation,useNavigate, useParams} from "react-router-dom";
+import VenueDataService from "../services/VenueDataService";
+import { useSelector, useDispatch } from "react-redux";
 function AddComment() {
+  let {id}=useParams();
   let location = useLocation();
+  let dispatch=useDispatch();
+  let navigate=useNavigate();
   const onSubmit = (evt) => {
     evt.preventDefault();
+  if(evt.target.elements.author.value &&
+    evt.target.elements.rating.value &&
+    evt.target.elements.text.value)
+  {
+    let newComment={
+      author:evt.target.elements.author.value,
+      rating:evt.target.elements.rating.value,
+      text:evt.target.elements.text.value
+    }
+    VenueDataService.addComment(id,newComment).then(
+      function(){
+        dispatch({"type":"ADD_COMMENT_SUCCESS"})
+        navigate("/venue/"+id);
+      }
+    );
+    }
   };
     return (
       <>
@@ -16,6 +37,16 @@ function AddComment() {
               id="yorumEkle"
               onSubmit={(evt) => onSubmit(evt)}
             >
+                 <div className="form-group">
+                <label className="col-sm-2 control-label">İsim:</label>
+                <div className="col-sm-10">
+                  <input type="text"
+                    className="form-control"
+                    name="author"
+                   
+                  />
+                </div>
+              </div>
               <div className="form-group">
                 <label className="col-xs-10 col-sm-2 control-label">
                   Puan:
--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -2,18 +2,39 @@ import InputWithLabel from "./InputWithLabel";
 import VenueList from "./VenueList";
 import Header from "./Header";
 import React from "react";
+import VenueDataService from "../services/VenueDataService";
+import { useSelector, useDispatch } from "react-redux";
 const Home = () => {
-   //Şimdilik veri statik. Backend bitince Rest API'den gelecek.
-  const venues = [
-    {
-      _id: 1,
-      name: "Bilgisayar Mühendisliği",
-      rating: 5,
-      distance: 1,
-      address: "SDÜ",
-      foodanddrink: ["Web", "Yazılım", "ASY"],
-    },
-  ];
+  //Şimdilik veri statik. Backend bitince Rest API'den gelecek.
+  const [coordinate, setCoordinate] = React.useState({ lat: 1, long: 1 });
+  const dispatch = useDispatch();
+  const venues = useSelector((state) => state.data);
+  const isError = useSelector((state) => state.isError);
+  const isSuccess = useSelector((state) => state.isSuccess);
+  const isLoading = useSelector((state) => state.isLoading);
+
+  React.useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        setCoordinate({
+          lat: position.coords.latitude,
+          long: position.coords.longitude,
+        });
+      });
+    }
+  }, []);
+
+  React.useEffect(() => {
+    dispatch({ type: "FETCH_INIT" });
+    VenueDataService.nearbyVenues(coordinate.lat, coordinate.long)
+      .then(function (response) {
+        dispatch({ type: "FETCH_SUCCESS", payload: response.data });
+      })
+      .catch(() => {
+        dispatch({ type: "FETCH_FAILURE" });
+      });
+  }, [coordinate.lat, coordinate.long]);
+
   return (
     <div>
       <Header
@@ -30,7 +51,15 @@ const Home = () => {
       />
       <hr />
       <div className="row">
-        <VenueList venues={venues} />
+        {isError ? (
+          <p>Veri alınamadı</p>
+        ) : isLoading ? (
+          <p>Veriler yükleniyor</p>
+        ) : (
+          isSuccess && (
+            <VenueList venues={Array.isArray(venues) ? venues : []} />
+          )
+        )}
       </div>
     </div>
   );
